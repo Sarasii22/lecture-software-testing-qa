a1a5ddccad5c7bb034435e4995a9e4a1ea4b774b
Sarasi Madahasi <183228383+Sarasii22@users.noreply.github.com>
2025-05-10 06:31:11 +0000

    calculator

---
+++ app/src/main/java/org/example/Calculator.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Calculator {
+
+  /*   private int x;
+    private int y;
+
+    public Calculator()
+    {
+        x = 0;
+        y = 0;
+    }
+
+    public void getValue(int a, int b)
+    {
+        
+    }
+
+    public int setValue()
+    {
+        
+        
+    }
+*/
+    public int add(int a, int b){
+        
+        return a + b;
+    }
+
+
+}
